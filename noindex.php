ddf659a7c399ab7de94965872e286e42168f1877
filvock <filvock@gmail.com>
2019-05-16 19:55:48 -0300

    noindex

---
+++ noindex.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Página temporária - sob construção</title>
+</head>
+<body>
+    
+<a href="www.igrejafontedevida.com.br/tesouraria.php">Clique aqui para acessar a tesouraria</a>.
+
+</body>>
+
