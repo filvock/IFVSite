9d2083a3366fb98119f25ce1881e369e9ad136a4
filvock <filvock@gmail.com>
2019-05-16 18:47:53 -0300

    Update AdminController.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -14,6 +14,6 @@ class AdminController extends Controller
     public function index()
     {
         $user = Auth()->User();
-        return view('Admin.index', compact('user'));
+        return view('admin.index', compact('user'));
     }
 }
